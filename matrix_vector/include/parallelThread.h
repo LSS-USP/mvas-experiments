cbcd4a8eef98c2c19b2a5108ed50c67b90c0943b
Siqueira <rodrigosiqueiramelo@gmail.com>
2017-02-10 09:28:46 -0200

    Added pthread handlers

--- matrix_vector/include/parallelThread.h
+++ matrix_vector/include/parallelThread.h
@@ -1,6 +1,27 @@
 #ifndef __PARALLEL_THREADS__
 #define __PARALLEL_THREADS__
 
-extern void * matrixVectorThread(void * pId);
+#include <pthread.h>
+
+#include "common.h"
+
+/**
+* Kernel function responsible to calculate just a small part of dot product
+*/
+extern void * matrixVectorKernel(void * pId);
+
+/**
+* Basically initialize threads data structure
+* @param pTotalThreads Total of threads to be created
+* @return Return a reference to array of pthread_t
+*/
+extern pthread_t * initThreadData(int pTotalThreads);
+
+/**
+* Start matrix multiplication
+* @param pTotalThreads
+* @param pInfo
+*/
+extern void matrixVectorThread(int pTotalThreads, dotProductData * pInfo);
 
 #endif
--- matrix_vector/src/parallelThread.c
+++ matrix_vector/src/parallelThread.c
@@ -4,29 +4,86 @@
 #include <pthread.h>
 
 #include "common.h"
+#include "parallelThread.h"
 
 // Matrix data
-static long * gMatrix = NULL;
-static long * gVector = NULL;
-static long * gFinalVector = NULL;
 static int gTotalThreads = 0;
-static int gMatrixLines = 0;
-static int gColumns = 0;
-static int gLines = 0;
+static dotProductData * infoThread = NULL;
 
-void * matrixVectorThread(void * pId)
+pthread_t * initThreadData(int pTotalThreads)
+{
+  if (pTotalThreads < 1)
+  {
+    printf("It is required at least one thread\n");
+    return NULL;
+  }
+  pthread_t * threadData = (pthread_t *)calloc(pTotalThreads,
+                                               sizeof(pthread_t));
+  if (!threadData)
+  {
+    printf("Problem to allocate pthread_t\n");
+    return NULL;
+  }
+  gTotalThreads = pTotalThreads;
+  return threadData;
+}
+
+void matrixVectorThread(int pTotalThreads, dotProductData * pInfo)
+{
+  if (!pInfo || pTotalThreads < 1)
+  {
+    printf("Invalid parameters\n");
+    return;
+  }
+  pthread_t * threadData = initThreadData(pTotalThreads);
+  if (!threadData)
+  {
+    printf("matrixVectorThread cannot allocate data structure\n");
+    return;
+  }
+
+  int status = 0;
+  infoThread = pInfo;
+  for (int thread = 0; thread < gTotalThreads; thread++)
+  {
+    status = pthread_create(&threadData[thread], NULL,
+                            matrixVectorKernel, (void*)&thread);
+    if (status)
+    {
+      printf("Error when tried to create thread: %d\n", thread);
+      continue;
+    }
+  }
+
+  for (int thread = 0; thread < gTotalThreads; thread++)
+  {
+    status = pthread_join(threadData[thread], NULL);
+    if (status)
+    {
+      printf("Error when tried to join thread: %d\n", thread);
+      continue;
+    }
+  }
+
+  free(threadData);
+  return;
+}
+
+void * matrixVectorKernel(void * pId)
 {
   long localId = *((long*)pId);
-  int localToProcess = gMatrixLines / gTotalThreads;
+  int localToProcess = infoThread->lines / gTotalThreads;
   int startLine = localId * localToProcess;
   int endLine = (localId + 1) * localToProcess - 1;
 
+  long tempElement = 0;
   for (int line = startLine; line <= endLine; line++)
   {
-    gFinalVector[line] = 0.0;
-    for (int column = 0; column < gColumns; column++)
+    infoThread->finalVector[line] = 0.0;
+    for (int column = 0; column < infoThread->columns; column++)
     {
-      gFinalVector[line] += *((gMatrix + line*gLines) + column) * gVector[column];
+      tempElement = *((infoThread->matrix + line*infoThread->lines) + column);
+      infoThread->finalVector[line] += tempElement * infoThread->vector[column];
     }
   }
 
