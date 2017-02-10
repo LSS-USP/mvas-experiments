653518ee36d178cce005e7596cfffbbf3e0bbfb7
Siqueira <rodrigosiqueiramelo@gmail.com>
2017-02-10 07:31:41 -0200

    Added parallel processing

---
+++ matrix_vector/src/parallelThread.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include <pthread.h>
+
+#include "common.h"
+
+// Matrix data
+static long * gMatrix = NULL;
+static long * gVector = NULL;
+static long * gFinalVector = NULL;
+static int gTotalThreads = 0;
+static int gMatrixLines = 0;
+static int gColumns = 0;
+static int gLines = 0;
+
+void * matrixVector(void * pId)
+{
+  long localId = *((long*)pId);
+  int localToProcess = gMatrixLines / gTotalThreads;
+  int startLine = localId * localToProcess;
+  int endLine = (localId + 1) * localToProcess - 1;
+
+  for (int line = startLine; line <= endLine; line++)
+  {
+    gFinalVector[line] = 0.0;
+    for (int column = 0; column < gColumns; column++)
+    {
+      gFinalVector[line] += *((gMatrix + line*gLines) + column) * gVector[column];
+    }
+  }
+
+  return NULL;
+}
+
+//int main(int argc, char * argv[])
+//{
+//  
+//  return 0;
+//}
