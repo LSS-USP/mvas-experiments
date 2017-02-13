34ad614ca458908926e789cda75933c5c3a64ebb
Siqueira <rodrigosiqueiramelo@gmail.com>
2017-02-13 09:22:03 -0200

    Added extra info to piData structure

--- estimate_pi/include/common.h
+++ estimate_pi/include/common.h
@@ -1,12 +1,16 @@
 #ifndef __COMMON__
 #define __COMMON__
 
+#include <pthread.h>
+
 #define BUFFER 4096
 
 typedef struct _piData
 {
   unsigned long terms;
   double pi;
+  double sum;
+  pthread_mutex_t mutex;
 } piData;
 
 extern void dumpEstimatePi(const piData * pInfo, const char * pBaseName);
