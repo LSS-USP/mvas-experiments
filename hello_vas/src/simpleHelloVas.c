bace943bd41b0c31c438c6ac0f0315abdf446bf1
Siqueira <rodrigosiqueiramelo@gmail.com>
2017-02-27 16:57:23 -0300

    Added simple Hello world

--- hello_vas/src/simpleHelloVas.c
+++ hello_vas/src/simpleHelloVas.c
@@ -19,61 +19,72 @@ typedef struct _vasAndSegment
   segid_t segment;
   char * vasName;
   char * segmentName;
-  void * commonArea;
+  int commonArea;
   unsigned long start;
   unsigned long size;
   mode_t permissions;
 } vasAndSegment;
 
-int vasWithSegment(vasAndSegment * pVasSegment)
+int createVAS(vasAndSegment * pVasSegment)
 {
   vasid_t vasId = 0;
-  segid_t segmentId = 0;
 
   vasId = vas_find(pVasSegment->vasName);
   if (vasId < 0)
   {
-   // Step 1: Create vas
-   vasId = vas_create(pVasSegment->vasName, segmentMode);
-   if (vasId < 0)
-   {
-     printf("Error on vas creation: %s\n", strerror(errno));
-     return -1;
-   }
+    // Step 1: Create vas
+    vasId = vas_create(pVasSegment->vasName, pVasSegment->permissions);
+    if (vasId < 0)
+    {
+      printf("Error on vas creation: %s\n", strerror(errno));
+      return -1;
+    }
   }
   else
   {
-    printf("VAS already created\n");
+    printf("VAS already exists. Continue...\n");
   }
+
   pVasSegment->vas = vasId;
+
+  return 0;
+}
+
+int createSegment(vasAndSegment * pVasSegment)
+{
+  segid_t segmentId = 0;
 
   segmentId = segment_find(pVasSegment->segmentName);
   if (segmentId < 0)
   {
     // Step 2: Create segment
     segmentId = segment_create_sz(pVasSegment->segmentName, pVasSegment->start,
-                                  pVasSegment->size, segmentMode);
+                                  pVasSegment->size, pVasSegment->permissions);
     if (segmentId < 0)
     {
       printf("Error on segment creation: %s\n", strerror(errno));
-      vas_delete(pVasSegment->vas);
       return -1;
     }
   }
   else
   {
-    printf("Segment already created\n");
+    printf("Segment already created. Continue...\n");
   }
 
-  // Step 3: Attach segment to vas
+  pVasSegment->segment = segmentId;
+
+  return 0;
+}
+
+int attachSegmentToVAS(vasAndSegment * pVasSegment)
+{
   int status = 0;
-  status = segment_attach(pVasSegment->vas, pVasSegment->segment, O_WRONLY);
+  status = segment_attach(pVasSegment->vas, pVasSegment->segment, O_RDWR);
   if (status < 0)
   {
     printf("Error on segment attach: %s\n", strerror(errno));
     return -1;
   }
-
   return 0;
 }
 
@@ -86,7 +97,7 @@ int playWithMvas(pid_t pPID, vasAndSegment * pVasSegment, int pValue)
     return -1;
   }
 
-  printf("TRY TO SWITCH\n");
+  printf("Try to switch\n");
   status = vas_switch(pVasSegment->vas);
   if (status < 0)
   {
@@ -94,15 +105,22 @@ int playWithMvas(pid_t pPID, vasAndSegment * pVasSegment, int pValue)
     return -1;
   }
 
-  printf("TRY TO MALLOC\n");
-  pVasSegment->commonArea = malloc(sizeof(int));
+  printf("Try to save data on new VAS :)\n");
+  pVasSegment->commonArea = 42;
   if (!pVasSegment->commonArea)
   {
-    perror("malloc");
+    perror("common area");
     return -1;
   }
-  *((int*)pVasSegment->commonArea) = pValue;
-  printf("This is VAS number: %d\n", * ((int*)pVasSegment->commonArea));
+
+  printf("This is the value on VAS: %d\n", pVasSegment->commonArea);
+
+  status = vas_switch(0);
+  if (status < 0)
+  {
+    printf("Cannot attach vas. Error: %s\n", strerror(errno));
+    return -1;
+  }
 
   status = vas_detach(pPID, pVasSegment->vas);
   if (status < 0)
@@ -119,67 +137,66 @@ int cleanMvas(vasAndSegment * pVasSegment, pid_t pPID)
   if (status < 0)
   {
     printf("Error to detach segment (%s)\n", pVasSegment->segmentName);
+    printf("Error: %s\n", strerror(errno));
   }
 
   status = segment_delete(pVasSegment->segment);
   if (status < 0)
   {
     printf("Error to delete segment (%s)\n", pVasSegment->segmentName);
+    printf("Error: %s\n", strerror(errno));
   }
 
   status = vas_delete(pVasSegment->vas);
   if (status < 0)
   {
     printf("Cannot delete vas. Error: %s\n", strerror(errno));
-  }
-
-  status = vas_delete(pVasSegment->vas);
-  if (status < 0)
-  {
-    printf("Problem to delete. Error: %s\n", strerror(errno));
+    printf("Error: %s\n", strerror(errno));
   }
   return 0;
 }
 
 int main(int argc, char * argv[])
 {
-  char vasName[BUFFERSIZE], segmentName[BUFFERSIZE];
-  vasAndSegment mvas[TOTAL_MVAS];
-
+  vasAndSegment mvas;
   pid_t taskPid = getpid();
   unsigned long start = BASE_ADDRESS;
   unsigned long size = BASE_SIZE;
   mode_t mode = S_IRWXU | S_IRGRP;
+
   // Step 1: Create VAS
-  printf("Initialization\n");
-  for (int i = 0; i < TOTAL_MVAS; i++)
+  printf("Initialization:  Get basic information\n");
+  mvas.vasName = "VAS0";
+  mvas.segmentName = "SEGMENT0";
+  mvas.start = start;
+  mvas.size = size;
+  mvas.permissions = mode;
+  printf("\tCreate VAS\n");
+  if (createVAS(&mvas) < 0)
   {
-    sprintf(vasName, "VAS%d", i);
-    sprintf(segmentName, "SEGMENT%d", i);
-    mvas[i].vasName = (char*)malloc(strlen(vasName) * sizeof(char));
-    mvas[i].segmentName = (char*)malloc(strlen(segmentName) * sizeof(char));
-    strcpy(mvas[i].vasName, vasName);
-    strcpy(mvas[i].segmentName, segmentName);
-    mvas[i].start = start;
-    mvas[i].size = size;
-    mvas[i].permissions = mode;
-    vasWithSegment(&mvas[i]);
-    start = start + size + 1;
+    return -1;
   }
 
-  // Step 2: Attach VAS to process
-  for (int i = 0; i < TOTAL_MVAS; i++)
+  printf("\tCreate Segment\n");
+  if (createSegment(&mvas) < 0)
   {
-    printf("====== VAS: %d ======\n", i);
-    printf("TRY TO ATTACH\n");
-    playWithMvas(taskPid, &mvas[i], i);
+    return -1;
   }
 
-  // Step 3: Clean everything
-  for (int i = 0; i < TOTAL_MVAS; i++)
+  printf("\tCreate attach segment to VAS\n");
+  if (attachSegmentToVAS(&mvas) < 0)
   {
-    cleanMvas(&mvas[i], taskPid);
+    return -1;
   }
+
+  printf("Try to attach\n");
+  if (playWithMvas(taskPid, &mvas, 1) < 0)
+  {
+    return -1;
+  }
+
+  printf("Clean everything\n");
+  cleanMvas(&mvas, taskPid);
 
   return 0;
 }
